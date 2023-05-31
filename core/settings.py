8c2700ab23219612d1a3a3bf2b4cd2504aa882a8
SebastianMou <Sebastian_Mou@protonmail.com>
2023-05-30 21:53:28 -0600

    images in inbox and directs

--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'chat.apps.ChatConfig',
 ]
 
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
